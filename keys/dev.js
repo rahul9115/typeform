b3e74dfef8e8fde85f87ffa38ce24bccf402e024
unknown <sudharahul2010@gmail.com>
2020-07-24 21:48:03 +0530

    complete production

---
+++ keys/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+    clientID: "679163885433-n15sv2bc33ngfbag862792kac6uiebfu.apps.googleusercontent.com",
+    clientSecret: "9d0qjZpelRfjkNHB6WrtZk2D",
+    callbackURL: "/auth/google/callback",
+    cookiekey: "pp",
+    stripePublishableKey: 'pk_test_51GxcfSFRpHIp8ryKMeLNpMThYcDvwbVCGbVMvEhkXDCiaR70rYnh2kC7dJGTD6rn5WQkeuNehmpOPdNpDuYZgm1800K3y5zOqD',
+    stripeSecretKey: 'sk_test_51GxcfSFRpHIp8ryK0JgbZAWhVh3Dei1jAbIVCEeZ4HbfTuBnlL94ZXfCxx8AWiN3Xqg02XAdP1xrjHxLFcKFIuqK00FNCfR7qt',
+    mongoURI: "mongodb+srv://rahul:rahul@cluster0.rpfjy.mongodb.net/<dbname>?retryWrites=true&w=majority"
+}

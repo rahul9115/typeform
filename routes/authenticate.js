7e1c47b24b0756ef282104e7ebe1c5351e635f47
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2020-07-25 00:16:16 +0530

    Update authenticate.js

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -2,10 +2,10 @@ const passport = require('passport');
 const cors = require('cors')
 module.exports = (app) => {
 
-    app.get('/auth/google', passport.authenticate('google', {
+    app.get('https://intense-badlands-91337.herokuapp.com/auth/google', passport.authenticate('google', {
         scope: ['email', 'profile']
     }))
-    app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
+    app.get('https://intense-badlands-91337.herokuapp.com/auth/google/callback', passport.authenticate('google'), (req, res) => {
         res.redirect('/surveys');
 
     }
@@ -13,11 +13,11 @@ module.exports = (app) => {
     
 
     );
-    app.get("/api/logout", (req, res) => {
+    app.get("https://intense-badlands-91337.herokuapp.com/api/logout", (req, res) => {
         req.logout();
         res.redirect('/');
     });
-    app.get('/api/output', (req, res) => {
+    app.get('https://intense-badlands-91337.herokuapp.com/api/output', (req, res) => {
         res.send(req.user);
     });
     app.use(cors());

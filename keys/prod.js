7247bca8cde5e00b0acc720b8531a96c4241e6f4
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2020-07-25 22:00:02 +0530

    Update prod.js

--- keys/prod.js
+++ keys/prod.js
@@ -3,5 +3,6 @@ module.exports = {
     clientSecret:  process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "/auth/google/callback",
     cookiekey: process.env.COOKIE_KEY, 
-     mongoURI: process.env.MONGODB_URI
+     mongoURI: process.env.MONGODB_URI,
+    absoluteURI:process.env. absoluteURI
 }

2ec00a0f315eea1c17233a34396df6634d067f57
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2020-07-26 22:28:09 +0530

    Update authreducers.js

--- client/src/reducers/authreducers.js
+++ client/src/reducers/authreducers.js
@@ -3,7 +3,7 @@ export default function (state = null, action) {
     console.log(action);
     switch (action.type) {
         case FETCH_USER:
-            return action.payload || true;
+            return action.payload || false;
 
         default:
             return state;
